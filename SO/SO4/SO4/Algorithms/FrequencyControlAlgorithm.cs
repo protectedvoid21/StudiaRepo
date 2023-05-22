841f1faa7203fc5ef3d0a5ef3b04acd7602f9d7d
protectedvoid21 <nieznanyzero@interia.pl>
2023-05-22 13:24:01 +0200

    Dodanie algorytmu częstości błędów stron [SO4]

---
+++ SO/SO4/SO4/Algorithms/FrequencyControlAlgorithm.cs
@@ -0,0 +1,56 @@
+namespace SO4.Algorithms;
+
+public class FrequencyControlAlgorithm : ProportionalAlgorithm {
+    private readonly float minFaultFrequencyThreshold;
+    private readonly float maxFaultFrequencyThreshold;
+    private readonly int faultWaitingTime;
+
+    private List<Page> freePages = new();
+
+    public FrequencyControlAlgorithm(
+        AlgorithmData data, string name, float minFaultFrequencyThreshold, float maxFaultFrequencyThreshold,
+        int faultWaitingTime) : base(data, name) {
+        this.minFaultFrequencyThreshold = minFaultFrequencyThreshold;
+        this.maxFaultFrequencyThreshold = maxFaultFrequencyThreshold;
+        this.faultWaitingTime = faultWaitingTime;
+    }
+
+    private void AdjustProcessPages(Process process) {
+        var pageFaultFrequency = process.PageFaultFrequency;
+        if (pageFaultFrequency < minFaultFrequencyThreshold) {
+            if (process.Pages.Count <= 1) {
+                return;
+            }
+
+            Page removedPage = process.RemovePage();
+            removedPage.SetReference(0, 0);
+            freePages.Add(removedPage);
+        }
+        else if (pageFaultFrequency > maxFaultFrequencyThreshold && freePages.Count > 0) {
+            process.Pages.Add(freePages[0]);
+            freePages.Remove(freePages[0]);
+        }
+    }
+
+    public override void Execute() {
+        SetProcessesPages();
+
+        while (tick < tickCount) {
+            tick++;
+            var recentlyCompletedProcesses = new List<Process>();
+            foreach (var process in processList) {
+                process.Execute(tick);
+                
+                if (process.RequestQueue.Count == 0) {
+                    recentlyCompletedProcesses.Add(process);
+                    CompletedProcesses.Add(process);
+                }
+                AdjustProcessPages(process);
+            }
+
+            foreach (var process in recentlyCompletedProcesses) {
+                processList.Remove(process);
+            }
+        }
+    }
+}
--- SO/SO4/SO4/Process.cs
+++ SO/SO4/SO4/Process.cs
@@ -2,37 +2,60 @@ namespace SO4;
 
 public class Process {
     public Queue<int> RequestQueue { get; init; }
-    public List<Page> Pages { get; set; } = new();
+    public readonly List<Page> Pages = new();
     public int FailureCount { get; private set; }
     public int MemorySize => RequestQueue.Distinct().Count();
 
-    public Process(Queue<int> requestQueue) {
+    private Queue<bool> faultQueue = new();
+    public int FaultTimeSpan { get; }
+    
+    public float PageFaultFrequency => (float)faultQueue.Count(f => f) / FaultTimeSpan;
+
+    public Process(Queue<int> requestQueue, int faultTimeSpan) {
         RequestQueue = requestQueue;
+        FaultTimeSpan = faultTimeSpan;
     }
     
     private Page GetAvailablePage(int request) {
-        Page nonUsedLongest = Pages[0];
+        Page longestUnused = Pages[0];
         
         foreach (var page in Pages) {
             if (page.Request == request) {
+                faultQueue.Enqueue(false);
                 return page;
             }
             if (page.IsEmpty) {
                 FailureCount++;
+                faultQueue.Enqueue(true);
                 return page;
             }
-            if (page.LastUsed < nonUsedLongest.LastUsed) {
-                nonUsedLongest = page;
+            if (page.LastUsed < longestUnused.LastUsed) {
+                longestUnused = page;
             }
         }
 
         FailureCount++;
-        return nonUsedLongest;
+        faultQueue.Enqueue(true);
+        return longestUnused;
     }
 
     public void Execute(int tick) {
         int request = RequestQueue.Dequeue();
         var currentPage = GetAvailablePage(request);
         currentPage.SetReference(request, tick);
+    }
+
+    public Page RemovePage() {
+        Page longestUnused = Pages[0];
+
+        foreach (var page in Pages) {
+            if (page.LastUsed < longestUnused.LastUsed) {
+                longestUnused = page;
+            }
+            if (page.IsEmpty) {
+                return page;
+            }
+        }
+        return longestUnused;
     }
 }
