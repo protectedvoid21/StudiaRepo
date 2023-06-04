d07a236de031ee903145bc9e8f8f869dd1bcdf51
protectedvoid21 <nieznanyzero@interia.pl>
2023-06-04 18:58:13 +0200

    [SO] Chyba skończyłem xdddd

--- SO/SO5/Algorithms/StrategyThird.cs
+++ SO/SO5/Algorithms/StrategyThird.cs
@@ -2,11 +2,25 @@ namespace SO5.Algorithms;
 
 public class StrategyThird : Algorithm {
     private int _transferPercentage;
-    private List<Processor> _freeProcessList = new();
+    private int _minThreshold;
 
-    public StrategyThird(AlgorithmData data, string name, int threshold, int transferPercentage) : base(data, name,
+    public StrategyThird(AlgorithmData data, string name, int threshold, int minThreshold, int transferPercentage) : base(data, name,
         threshold) {
         _transferPercentage = transferPercentage;
+        _minThreshold = minThreshold;
+    }
+
+    private void DistributeFromLightToOverloaded() {
+        foreach (var processor in Processors) {
+            if (processor.AskForUsage() < _minThreshold) {
+                var overloadedProcessor = Processors.FirstOrDefault(p => p.AskForUsage() > _threshold);
+                if (overloadedProcessor == null) {
+                    return;
+                }
+                
+                overloadedProcessor.TransferRequests(processor, _transferPercentage);
+            }
+        }
     }
     
     public override void Execute() {
@@ -18,6 +32,39 @@ public class StrategyThird : Algorithm {
                     processor.Execute();
                 }
                 tick++;
+            }
+            
+            int sourceId = request.ProcessorId;
+            if(Processors[sourceId].AskForUsage() <= _threshold) {
+                Processors[sourceId].AddRequest(request.Amount);
+
+                foreach (var processor in Processors) {
+                    processor.Execute();
+                }
+                continue;
+            }
+
+            var restIds = GetRestIds(sourceId);
+
+            bool found = false;
+            while (restIds.Count > 0) {
+                var foundProcess = Processors[GetRandomProcessId(restIds)];
+
+                if (foundProcess.AskForUsage() <= _threshold) {
+                    foundProcess.AddRequest(request.Amount);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                Processors[sourceId].AddRequest(request.Amount);
+            }
+            
+            DistributeFromLightToOverloaded();
+            
+            foreach(var processor in Processors) {
+                processor.Execute();
             }
         }
     }
