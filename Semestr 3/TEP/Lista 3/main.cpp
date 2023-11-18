c1d31e91e455376d230f9b52f0a82d06d486f4f3
protectedvoid21 <nieznanyzero@interia.pl>
2023-11-18 23:01:31 +0100

    [TEP] Lista 3 - dodanie main i cmake

---
+++ Semestr 3/TEP/Lista 3/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "TreeManager.h"
+#include "Tree.h"
+#include "Operations.h"
+
+void testOperators() {
+    std::map<std::string, Operation *> operations;
+    operations["+"] = new AddOperation();
+    operations["-"] = new SubtractOperation();
+    operations["*"] = new MultiplyOperation();
+    operations["/"] = new DivideOperation();
+    operations["sin"] = new SinOperation();
+    operations["cos"] = new CosOperation();
+    
+    auto tree1Input = splitByWhitespace("+ + 3 5 * 2 4");
+    auto tree2Input = splitByWhitespace("* - 10 1 + 5 1");
+    
+    Tree tree1(tree1Input, operations);
+    Tree tree2(tree2Input, operations);
+    
+    tree1.buildTree();
+    tree2.buildTree();
+    
+    std::cout << "Tree built." << std::endl;
+    
+    //Tree tree3 = tree1 + tree2;
+    //std::cout << tree3.print() << std::endl;
+}
+
+int main() {
+    //testOperators();
+    std::map<std::string, Operation *> operations;
+    operations["+"] = new AddOperation();
+    operations["-"] = new SubtractOperation();
+    operations["*"] = new MultiplyOperation();
+    operations["/"] = new DivideOperation();
+    operations["sin"] = new SinOperation();
+    operations["cos"] = new CosOperation();
+
+    std::string input;
+    
+    TreeManager treeManager(operations);
+
+    while (input != "exit") {
+        std::cout << "Enter command: ";
+        std::getline(std::cin, input);
+        treeManager.getInput(input);
+    }
+}
